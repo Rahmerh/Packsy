1d4b3d7bafa0de648873b0811bb65c235a31dc65
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-06-12 11:11:44 +0200

    List packages implemented

---
+++ src/cli.rs
@@ -0,0 +1,13 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(name = "packsy", version, about = "Declarative package manager MVP")]
+pub struct PacksyCli {
+    #[command(subcommand)]
+    pub command: Option<Command>,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    List,
+}
